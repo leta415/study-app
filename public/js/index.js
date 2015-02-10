d153fd8185e5ee641f17cbb408d4688668575ca7
Leta He <leta.he.415@gmail.com>
2015-02-10 11:23:06 +0000

    Default study locations added. Results panel section started. Added heroku repo.

--- public/js/index.js
+++ public/js/index.js
@@ -1,75 +1,102 @@
+// Default study spot locations
+geisel = new google.maps.LatLng(32.88120, -117.23750);
+cselabs = new google.maps.LatLng(32.88180, -117.23352);
+priceCenter = new google.maps.LatLng(32.87967, -117.23653);
+apm = new google.maps.LatLng(32.87903, -117.24103);
+biomed = new google.maps.LatLng(32.87665, -117.23701);
+defaultLocations = [geisel, cselabs, priceCenter, apm, biomed];
+/////////////////////////////////
+
 // Initialize map
 (function() {
 
-if(!!navigator.geolocation) {
+  if(!!navigator.geolocation) {
 
-  var map;
-
-  var mapOptions = {
-    zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  navigator.geolocation.getCurrentPosition(function(position) {
-  
-    var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var mapOptions = {
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
     
-    var infowindow = new google.maps.InfoWindow({
-      map: map,
-      position: geolocate,
-      content:
-        '<h1>Location pinned from HTML5 Geolocation!</h1>' +
-        '<h2>Latitude: ' + position.coords.latitude + '</h2>' +
-        '<h2>Longitude: ' + position.coords.longitude + '</h2>'
+    //Get HTML element and set up the map
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+    
+      var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude); 
+
+      // Create info window for current location marker
+      origInfowindow = new google.maps.InfoWindow({
+        map: map,
+        position: geolocate,
+        content:
+          '<h1>Current location</h1>' /*+
+          '<h2>Latitude: ' + position.coords.latitude + '</h2>' +
+          '<h2>Longitude: ' + position.coords.longitude + '</h2>'*/
+      });
+      origInfowindow.close();
+      map.setCenter(geolocate);
+
+      // Create marker for current location
+      origMarker = new google.maps.Marker({
+        position: geolocate,
+        map: map,
+        title: 'You are here.',
+      });
+
+      // Add click listener for current location marker
+      google.maps.event.addListener(origMarker, 'click', function() {
+        origInfowindow.open(map, origMarker);
+      });
+      
     });
     
-    map.setCenter(geolocate);
-
-    var marker = new google.maps.Marker({
-      position: geolocate,
-      map: map,
-      title: 'Hello World!',
-    });
-    
-  });
-  
-} else {
-  document.getElementById('map-canvas').innerHTML = 'No Geolocation Support.';
-}
+  } else {
+    document.getElementById('map-canvas').innerHTML = 'No Geolocation Support.';
+  }
 
 })();
 
 
 // Initialize autocomplete places 
 function initialize() {
-  var mapOptions = {
-    center: new google.maps.LatLng(-33.8688, 151.2195),
-    zoom: 13
-  };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-  mapOptions);
 
   var input = /** @type {HTMLInputElement} */(
     document.getElementById('search-places-input'));
 
-  // var types = document.getElementById('type-selector');
-  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
-
   var autocomplete = new google.maps.places.Autocomplete(input);
   autocomplete.bindTo('bounds', map);
 
-  var infowindow = new google.maps.InfoWindow();
-  var marker = new google.maps.Marker({
+  // var startLocInfowindow = new google.maps.InfoWindow();
+  var startLocMarker = new google.maps.Marker({
     map: map,
-    anchorPoint: new google.maps.Point(0, -29)
+    // anchorPoint: new google.maps.Point(0, -29)
   });
 
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
-    infowindow.close();
-    marker.setVisible(false);
+    // Destroy original current location
+    if (origInfowindow) {
+      google.maps.event.clearInstanceListeners(origInfowindow);  // just in case handlers continue to stick around
+      origInfowindow.close();
+      origInfowindow = null;
+      origMarker.setVisible(false);
+    }
+
+    // Change html layout to show results 
+    var resultsPanelsElem = document.getElementById("results-panels");
+    resultsPanelsElem.className = "";
+    resultsPanelsElem.className = "col-xs-12 col-md-4";
+    var mapCanvasElem = document.getElementById("map-canvas");
+    mapCanvasElem.className = "";
+    mapCanvasElem.className = "col-xs-12 col-md-8";
+    resultsPanelsElem.innerHTML = 
+      "<div class='panel panel-primary'>" +
+        "<div class='panel-body'>" +
+          "Geisel Library" +
+        "</div>" + 
+      "</div>";
+
+    // infowindow.close();
+    startLocMarker.setVisible(false);
     var place = autocomplete.getPlace();
     if (!place.geometry) {
       return;
@@ -80,44 +107,42 @@ function initialize() {
       map.fitBounds(place.geometry.viewport);
     } else {
       map.setCenter(place.geometry.location);
-      map.setZoom(17);  // Why 17? Because it looks good.
+      map.setZoom(16);  // When in doubt, 17 looks good.
     }
-    marker.setIcon(/** @type {google.maps.Icon} */({
-      url: place.icon,
+
+    startLocMarker.setIcon(/** @type {google.maps.Icon} */({
+      // url: place.icon,
+      url: "/img/youarehere-button.png",
       size: new google.maps.Size(71, 71),
       origin: new google.maps.Point(0, 0),
       anchor: new google.maps.Point(17, 34),
       scaledSize: new google.maps.Size(35, 35)
     }));
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
 
-    var address = '';
-    if (place.address_components) {
-      address = [
-        (place.address_components[0] && place.address_components[0].short_name || ''),
-        (place.address_components[1] && place.address_components[1].short_name || ''),
-        (place.address_components[2] && place.address_components[2].short_name || '')
-      ].join(' ');
+    startLocMarker.setPosition(place.geometry.location);
+    startLocMarker.setVisible(true);
+
+    var index;
+    for (index = 0; index < defaultLocations.length; index++) {
+      var marker = new google.maps.Marker({
+        position: defaultLocations[index],
+        map: map
+      });
     }
+    // var address = '';
+    // if (place.address_components) {
+    //   address = [
+    //     (place.address_components[0] && place.address_components[0].short_name || ''),
+    //     (place.address_components[1] && place.address_components[1].short_name || ''),
+    //     (place.address_components[2] && place.address_components[2].short_name || '')
+    //   ].join(' ');
+    // }
 
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-    infowindow.open(map, marker);
-  });
+    // origInfowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    // origInfowindow.open(map, origMarker);
+  }); 
+  // End of addListener for autocomplete
 
-  // Sets a listener on a radio button to change the filter type on Places
-  // Autocomplete.
-  // function setupClickListener(id, types) {
-  //   var radioButton = document.getElementById(id);
-  //   google.maps.event.addDomListener(radioButton, 'click', function() {
-  //     autocomplete.setTypes(types);
-  //   });
-  // }
-
-  // setupClickListener('changetype-all', []);
-  // setupClickListener('changetype-address', ['address']);
-  // setupClickListener('changetype-establishment', ['establishment']);
-  // setupClickListener('changetype-geocode', ['geocode']);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
