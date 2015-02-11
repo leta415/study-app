7521f528fbcfcbd6355d0f8ea1bfbf4af0300c6b
jamiey108 <jamiey108@gmail.com>
2015-02-11 12:57:50 -0800

    fixed delete

--- public/js/delete-bookmarks.js
+++ public/js/delete-bookmarks.js
@@ -1,4 +1,4 @@
-/*'use strict';
+'use strict';
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
@@ -8,40 +8,17 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
-//function initializePage() {
-/*	$('#myModal').on('show', function() {
-		console.log("in show function");
-	    var tit = $('.confirm-delete').data('title');
-
-	    $('#myModal .modal-body p').html("You are about to delete bookmark " + '<b>' + tit +'</b>' + ' ?');
-	})*/
-
-/*	$('.confirm-delete').on('click', function(e) {
+function initializePage() {
+	$('a.btnDelete').on('click', function (e) {
 	    e.preventDefault();
-	    console.log("delete button clicked");
+	    var id = $(this).closest('tr').data('id');
+	    $('#myModal').data('id', id).modal('show');
+	});
 
-	    var id = $(this).data('id');
-	    console.log("id = " + id);
-	    $('#myModal').modal('show');
-	    //$('#myModal').data('id', id).modal('show');
-	}); 
-
-/*	$('#btnYes').click(function() {
-	    // handle deletion here
+	$('#btnDelteYes').click(function () {
 	    var id = $('#myModal').data('id');
-	    $('[data-id='+id+']').parents('tr').remove();
+	    $('[data-id=' + id + ']').remove();
 	    $('#myModal').modal('hide');
-    });*/
-//}
+	});
+}
 
-$('button.btnDelete').on('click', function (e) {
-    e.preventDefault();
-    var id = $(this).closest('tr').data('id');
-    $('#myModal').data('id', id).modal('show');
-});
-
-$('#btnDelteYes').click(function () {
-    var id = $('#myModal').data('id');
-    $('[data-id=' + id + ']').remove();
-    $('#myModal').modal('hide');
-});
