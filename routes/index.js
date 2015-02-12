497b450bd8dbb84aecef72ae180b41407aeac5d9
jamiey108 <jamiey108@gmail.com>
2015-02-12 15:44:03 -0800

    got rid of i'm feeling lucky, settings, and classes

--- routes/index.js
+++ routes/index.js
@@ -7,24 +7,9 @@ exports.view = function(req, res) {
   res.render('index', {
     "menu-items": [
       {
-        "name" : "Classes",
-        "img"  : "books.png",
-        "page" : "classes"
-      },
-      {
         "name" : "Recent Spots",
         "img"  : "recent.png",
         "page" : "recent"
-      },
-      {
-        "name" : "My Groups",
-        "img"  : "group.png",
-        "page" : "group"
-      },
-      {
-        "name" : "Friends",
-        "img"  : "friend.png",
-        "page" : "friends"
       },
       {
         "name" : "Check-ins",
@@ -32,24 +17,24 @@ exports.view = function(req, res) {
         "page" : "checkin"
       },
       {
-        "name" : "Map",
-        "img"  : "map.png",
-        "page" : "map"
+        "name" : "My Groups",
+        "img"  : "group.png",
+        "page" : "group"
       },
       {
         "name" : "Bookmarks",
         "img"  : "favorite.png",
         "page" : "bookmarks"
+      },    
+      {
+        "name" : "Map",
+        "img"  : "map.png",
+        "page" : "map"
       },
       {
-        "name" : "I'm feeling lucky",
-        "img"  : "lucky.png",
-        "page" : "lucky"
-      },
-      {
-        "name" : "Settings",
-        "img"  : "settings.png",
-        "page" : "settings"
+        "name" : "Friends",
+        "img"  : "friend.png",
+        "page" : "friends"
       }
     ]
   });
