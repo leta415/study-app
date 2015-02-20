8e45cc642f6c463c17bdb654eef5bc465970b347
Heather Lee <hal010@ucsd.edu>
2015-02-20 04:35:40 +0000

    Recent places is now synced with checkins

--- routes/recent.js
+++ routes/recent.js
@@ -1,6 +1,22 @@
+var config = require('../config.js'), //config file contains all tokens and other private info
+    db = require('orchestrate')(config.db); //config.db holds Orchestrate token
+
 // Get recent spot data
-var data = require('../public/json/recent.json');
+var data;
 
 exports.recentPlaces = function(req, res) {
-  res.render('recent', data);
+	db.get('checkins', req.user.username)
+	.then(function (result) {
+		data = result.body;
+		data.pageName = "Recent Spots";
+		res.render('recent', data);
+		console.log(result.body);
+	})
+	.fail(function (err) {
+		var data = require('../public/json/recent.json');
+		res.render('recent', data);
+		console.log("failed");
+	})
+
+  // res.render('recent', data);
 };
