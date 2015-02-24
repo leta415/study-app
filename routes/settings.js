69a5fd1be8c3977207a6b9caa918fd3a4ff03d54
Heather Lee <hal010@ucsd.edu>
2015-02-23 22:39:11 -0800

    Changed settings page

--- routes/settings.js
+++ routes/settings.js
@@ -1,5 +1,20 @@
+var config = require('../config.js'), //config file contains all tokens and other private info
+    db = require('orchestrate')(config.db); //config.db holds Orchestrate token
+
 exports.list = function(req, res) {
-  res.render('settings', {
-  	pageName: 'Settings'
-  });
+	// Find user's current settings in database
+	db.get('local-users', req.user.username)
+	.then(function (result) {
+		data = result.body;
+		data.pageName = "Settings";
+		res.render('settings', data);
+		console.log(result.body);
+	})
+	.fail(function (err) {
+		var empty;
+		empty = {"pageName": "Settings"};
+		res.render('settings', empty);
+		console.log("failed");
+	})
+
 };
